6f9e3712b39a9e0ee625eead2bba4ad34e5781aa
AlexandreLeonetti <al.leo@wanadoo.fr>
2024-04-28 17:21:25 +0200

    graph-search

---
+++ readme.md
@@ -0,0 +1,41 @@
+# fibonacci recursion examples using TypeScript 
+
+> graph search implemented on 2D matrix using TypeScript. 
+
+## Install
+
+```sh
+
+npm install @leonetti/graph-search
+
+```
+
+## Usage
+
+```js
+
+import {mazeSearch} from '@leonetti/graph-search';
+
+
+/* define the maze */
+
+const maze = [
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+];
+
+/* define start and end points row column */
+const start = { x: 0, y: 0 };
+const end = { x: 9, y: 9 };
+
+mazeSearch(maze, start, end);
+
+```
---
+++ source/index.ts
@@ -0,0 +1,88 @@
+/*
+let's implement Depth First Search recursion to find path in a 2D matrix
+containing obstacles
+*/
+
+/* types */
+
+type point = {
+	x : number;
+	y : number;
+}
+
+/* maze */
+const WHITE = 0;
+const BLACK = 1;
+
+/* define the maze */
+
+const maze = [
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+];
+
+/* define start and end points row column */
+const start = { x: 0, y: 0 };
+const end = { x: 9, y: 9 };
+
+export function mazeSearch(maze:number[][], s:point, e:point):point[]{
+	
+		let path : point[] = [];
+
+	function checker(p:point):boolean{
+		/* this function checks nodes just like in trees */
+        /* check if current coords are white and within boundaries */
+        /* if not return false */
+			if(!((0<=  p.x && p.x <maze.length )&& (0<= p.y && p.y<maze[p.x].length))){
+					return false;
+			}
+		if(maze[p.x][p.y]===BLACK)	{
+			return false;
+		}
+
+        /* we check the value of the node after ensuring it is within boundaries.
+         * note that && statements are evaluated from left to right;
+         */
+		
+
+
+		/* add current node to path and mark as black*/
+		path.push(p);
+		maze[p.x][p.y]=BLACK;
+
+
+        /* if we find the end, return true to indicate success */
+		if(p.x == e.x && p.y == e.y)	{
+			return true;
+		}
+		
+        /* recursively check neighboring coords */
+		let neighbors = [{x:p.x+1,y:p.y},{x:p.x-1,y:p.y},{x:p.x, y:p.y-1},{x:p.x, y:p.y+1}];
+		for (let n of neighbors){
+			if(checker(n)){
+				return true;
+			}
+		}
+		
+		
+        /* if no path found */
+		path.pop();
+		return false;
+	}
+
+		if(!checker(s)){
+			return [];
+		}
+
+		return path;
+}
+
+
